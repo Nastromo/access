ba2a9ace63bc5f46b4d932b75095a7562f081512
nastromo <nastromo@Solaris.local>
2019-06-07 12:06:48 +0300

    client details done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,105 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import DropDown from './DropDown';
+
+
 
 export class Main extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.dateOptions = [`Today`, `Yesterday`, `2 Days Ago`];
+        this.fastingOptions = [`Yes`, `No`, `Unknown`, `No Specimen`];
+        this.statOptions = [`Yes`, `No`];
+        this.orderType = [`IGS Initial`, `IGS Follow up`, `IGS Redraw`];
+    }
+
 
     render() {
         return (
-            <div>
-                Entry
+            <div className="padd15">
+                <p className="side-t">Client Details</p>
+                <div className="cli-det">
+                    <div>
+                        <p className="tit-ins">Client Location ID</p>
+                        <input className="simple-input" type="text" />
+                    </div>
+                    <div>
+                        <p className="tit-ins">Physician ID</p>
+                        <input className="simple-input" type="text" />
+                    </div>
+                    <div>
+                        <p className="tit-ins">Col. Date</p>
+                        <div className="fl-date">
+                            <div id="fir-da" className="sma">
+                                <DropDown
+                                    option={this.props.date}
+                                    status={this.props.isDateOpen}
+                                    menu={this.dateOptions}
+                                    id="date" />
+                            </div>
+                            <input className="simple-input sma" type="text" placeholder="HH:mm" />
+                        </div>
+                    </div>
+                    <div>
+                        <p className="tit-ins">Fasting</p>
+                        <div id="fir-da" className="sma">
+                            <DropDown
+                                option={this.props.fasting}
+                                status={this.props.isFastingOpen}
+                                menu={this.fastingOptions}
+                                id="fasting" />
+                        </div>
+                    </div>
+                    <div>
+                        <p className="tit-ins">Stat</p>
+                        <div id="fir-da" className="sma">
+                            <DropDown
+                                option={this.props.stat}
+                                status={this.props.isStatOpen}
+                                menu={this.statOptions}
+                                id="stat" />
+                        </div>
+                    </div>
+                    <div>
+                        <p className="tit-ins">Electronic Order Number</p>
+                        <input className="simple-input" type="text" />
+                    </div>
+                    <div>
+                        <p className="tit-ins">Order Type</p>
+                        <div id="fir-da" className="sma">
+                            <DropDown
+                                option={this.props.orderType}
+                                status={this.props.isOrderTypeOpen}
+                                menu={this.orderType}
+                                id="orderType" />
+                        </div>
+                    </div>
+                </div>
+                <div className="cli-det fl-sta">
+                    <div id="fir-diag">
+                        <p className="tit-ins">Diagnoses</p>
+                        <input className="simple-input widee" type="text" />
+                    </div>
+                    <div>
+                        <p className="tit-ins">Specimens</p>
+                        <input className="simple-input widee" type="text" />
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
 const mapStateToProps = (state) => ({
-    
+    date: state.dropdownOption.date,
+    isDateOpen: state.dropdownStatus.date,
+    fasting: state.dropdownOption.fasting,
+    isFastingOpen: state.dropdownStatus.fasting,
+    stat: state.dropdownOption.stat,
+    isStatOpen: state.dropdownStatus.stat,
+    orderType: state.dropdownOption.orderType,
+    isOrderTypeOpen: state.dropdownStatus.orderType,
+
 })
 
 const mapDispatchToProps = {
--- src/store/reducers/DropDown.js
+++ src/store/reducers/DropDown.js
@@ -1,12 +1,27 @@
+import moment from 'moment';
+
+
 export const dropdownStatus = (state = {}, action) => {
     let newState;
     if (action.type === `SHOW_DROP_DOWN`) {
         switch (action.obj.id) {
-            case `departament`:
+            case `date`:
                 newState = JSON.parse(JSON.stringify(state));
-                newState.departament = action.obj.status;
+                newState.date = action.obj.status;
                 return newState;
-            
+            case `fasting`:
+                newState = JSON.parse(JSON.stringify(state));
+                newState.fasting = action.obj.status;
+                return newState;
+            case `stat`:
+                newState = JSON.parse(JSON.stringify(state));
+                newState.stat = action.obj.status;
+                return newState;
+            case `orderType`:
+                newState = JSON.parse(JSON.stringify(state));
+                newState.orderType = action.obj.status;
+                return newState;
+
             default: return state;
         }
     } else {
@@ -18,12 +33,34 @@ export const dropdownOption = (state = {}, action) => {
     let newState;
     if (action.type === `SET_DROP_DOWN_OPTION`) {
         switch (action.obj.id) {
-            case `departament`:
+            case `date`:
                 newState = JSON.parse(JSON.stringify(state));
-                newState.departament = action.obj.option;
+                switch (action.obj.option) {
+                    case `Today`:
+                        newState.date = moment().format('MM/DD/YYYY');
+                        return newState;
+                    case `Yesterday`:
+                        newState.date = moment().subtract(1, "days").format('MM/DD/YYYY');
+                        return newState;
+                    case `2 Days Ago`:
+                        newState.date = moment().subtract(2, "days").format('MM/DD/YYYY');
+                        return newState;
+                    default: return newState;
+                }
+            case `fasting`:
+                newState = JSON.parse(JSON.stringify(state));
+                newState.fasting = action.obj.option;
                 return newState;
-            
-            default: return state;
+            case `stat`:
+                newState = JSON.parse(JSON.stringify(state));
+                newState.stat = action.obj.option;
+                return newState;
+            case `orderType`:
+                newState = JSON.parse(JSON.stringify(state));
+                newState.orderType = action.obj.option;
+                return newState;
+
+            default: return newState;
         }
     } else {
         return state;
